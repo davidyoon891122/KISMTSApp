9a309cf9e1bfcf41bf112933ae2753e543336b7c
Davidyoon <davidyoon@koreainvestment.com>
2023-09-19 15:42:54 +0900

    Add an accessTokenExpiredDate
    * Added an accessTokenExpiredDate on the AccessTokenResponseModel

--- KISMTSApp/Entity/AccessTokenResponseModel.swift
+++ KISMTSApp/Entity/AccessTokenResponseModel.swift
@@ -11,10 +11,12 @@ struct AccessTokenResponseModel: Codable {
     let accessToken: String
     let tokenType: String
     let expireDate: Int
+    let accessTokenExpiredDate: String
     
     enum CodingKeys: String, CodingKey {
         case accessToken = "access_token"
         case tokenType = "token_type"
         case expireDate = "expires_in"
+        case accessTokenExpiredDate = "access_token_token_expired"
     }
 }
